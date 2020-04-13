1d5e6e00feda51b6c1595810692a62a9fa455f9e
ashith <ashith.rahul@razorthink.com>
2020-04-13 16:36:07 +0530

    removes styled component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import '@babel/polyfill';
 import React from 'react';
-import { columns, initialData } from './Table.constants';
+import {columns, initialData} from './Table.constants';
 import Table from '../components/Table/Table';
-import styled from "styled-components";
-
-
-const Styles = styled.div`
-  padding: 1rem;
-
-  .table {
-    display: inline-block;
-    border-spacing: 0;
-    border: 1px solid black;
-
-    .tr {
-      :last-child {
-        .td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    .th,
-    .td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`;
 
 export const GlobalContext = React.createContext({});
 
 const App = () => (
-  <GlobalContext.Provider>
-      <Styles>
-          <Table columns={columns} data={initialData}/>
-      </Styles>
-  </GlobalContext.Provider>
+    <GlobalContext.Provider>
+        <Table
+            columns={columns}
+            data={initialData}/>
+    </GlobalContext.Provider>
 );
 export default App;
